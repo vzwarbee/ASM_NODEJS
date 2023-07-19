2c18908c6db5b389e3893364a1ff800da3dcffdc
vzwarbee <dovantai112344@gmail.com>
2023-07-19 17:46:40 +0700

    up season

--- src/component/lab3/Season.jsx
+++ src/component/lab3/Season.jsx
@@ -1,27 +1,27 @@
 const config = {
-    summer: {
-        text: "Nóng",
-        iconName: ""
-    },
-    winter: {
-        text: "Lạnh",
-        iconName: ""
-    },
-}
-export default function Season(prop) {
-  const {  month,lat } = prop;
- if(month > 2 && month <9){
-    return lat >= 0? "summer": "winter"
- }else {
-    return lat >= 0? "winter": "summer"
- }
+  summer: {
+    text: "Nóng",
+    iconName: "fa-regular fa-sun",
+    background: "#ff6a00", 
+  },
+  winter: {
+    text: "Lạnh",
+    iconName: "fa-regular fa-snowflake",
+    background: "#3494E6", 
+  },
+};
 
- const {text, iconName} = config({month, lat})
-  
+export default function Season(prop) {
+  const { month, lat } = prop;
+  const season = month > 2 && month < 9 ? (lat >= 0 ? "summer" : "winter") : lat >= 0 ? "winter" : "summer";
+  const { text, iconName, background } = config[season];
+
   return (
-    <div className="row season">
-        {text}
-        {iconName}
+    <div className="row position-relative">
+      <div className={`col-6 season font`} style={{backgroundColor: background}}>
+      {text}
+      <i className={`${iconName} fs-1`}></i>
+    </div>
     </div>
   );
 }
